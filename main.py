1d93a187fbd13439c907913dc6a261f191f12be5
Aom <aom2@gmail.com>
2026-05-08 12:25:17 +0700

    fix cors

--- main.py
+++ main.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 
 import models
 from database import engine
+from fastapi.middleware.cors import CORSMiddleware
 
 from routers import users
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 models.Base.metadata.create_all(bind=engine)
 
